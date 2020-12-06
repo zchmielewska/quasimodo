12a7b1ce013e9f3d1201db9846bb8a38b9def812
zchmielewska <zchmielewska@gmail.com>
2020-12-06 20:52:14 +0100

    manage settings

--- compare.py
+++ compare.py
@@ -1,31 +1,72 @@
 import numpy as np
 import pandas as pd
+import os
 from tkinter import messagebox as msg
 from datetime import datetime
+from pathlib import Path
 from utils import *
 
 
-def run_comparison(lhs_filepath, rhs_filepath):
-    # Filepath can't be empty
-    if lhs_filepath == "":
-        msg.showwarning("Empty filepath", "Path to the first file is empty. Please fill in the filepath.")
+def run_comparison(lhs, rhs, output, settings, new_settings):
+
+    # Output folder can't have space at the end
+    while output[-1] == " ":
+        output = output[:-1]
+
+    # New settings will be saved to settings.txt for future usage
+    new_settings['lhs_filepath'] = lhs
+    new_settings['rhs_filepath'] = rhs
+    new_settings['output_folder'] = output
+
+    # Paths need to be properly formatted
+    lhs_filepath = Path(lhs)
+    rhs_filepath = Path(rhs)
+    output_folder = Path(output)
+
+    # Filepath must exist
+    if not lhs_filepath.exists():
+        msg.showwarning("File does not exist", "The left file does not exist. Please try again.")
         return
 
-    if rhs_filepath == "":
-        msg.showwarning("Empty filepath", "Path to the second file is empty. Please fill in the filepath.")
+    if not rhs_filepath.exists():
+        msg.showwarning("File does not exist", "The right file does not exist. Please try again.")
         return
 
-    # todo What if files don't exist
+    if not output_folder.exists():
+        answer = msg.askyesno("Create output folder", "The output folder does not exist. Do you want to create it?")
+        if answer is True:
+            try:
+                os.makedirs(output_folder)
+                print(output_folder)
+            except OSError:
+                msg.showwarning("Creation of the directory %s failed" % str(output_folder))
+        else:
+            return
 
     output = compare(lhs_filepath, rhs_filepath)
+
+    # Results are stored in quasimodo_YYYYMMDD.xlsx file
+    # todo: what is user want output as csv
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
     filename = "quasimodo_" + timestamp + ".xlsx"
-    output.to_excel(filename, index = False)
+    filepath = output_folder / filename
+    output.to_excel(filepath, index = False)
+    msg.showinfo("Output file", "Created file:\n" + str(filepath))
+
+    # If users changed settings, they are written to text file
+    if new_settings != settings:
+        file = open("settings.txt", "w")
+        file.write(str(new_settings))
+        file.close()
 
 
 def compare(lhs_filepath, rhs_filepath):
+    # todo: what if separator is comma?
+    # todo: what if file is excel?
     lhs = pd.read_csv(lhs_filepath, sep=";")
     rhs = pd.read_csv(rhs_filepath, sep=";")
+
+    print(lhs)
 
     common_cols = np.intersect1d(np.array(lhs.columns), np.array(rhs.columns))
     lhs_only_cols = np.setdiff1d(np.array(lhs.columns), common_cols)
--- main.py
+++ main.py
@@ -1,10 +1,31 @@
+import ast
 import tkinter as tk
 from tkinter import ttk
 from compare import *
+from pathlib import Path
+
+# Load configuration
+# If settings file doesn't exist, create it; otherwise read it
+if not Path("settings.txt").exists():
+    file = open("settings.txt", "w+")
+    settings = {
+        'lhs_filepath': "",
+        'rhs_filepath': "",
+        'output_folder': ""
+    }
+    file.write(str(settings))
+    file.close()
+else:
+    file = open("settings.txt", "r")
+    contents = file.read()
+    settings = ast.literal_eval(contents)
+    file.close()
+
+# If user changes settings, they will be saved in txt file
+new_settings = settings.copy()
 
 # Create instance
 window = tk.Tk()
-# window.resizable(0, 0)
 
 # Add a title
 window.title("quasimodo | compare tables")
@@ -17,31 +38,37 @@ tab2 = ttk.Frame(tabControl)
 tabControl.add(tab2, text="Settings")
 
 # Left entry box
-lhs_filepath = tk.StringVar()
-tk.Label(tab1, text="First file:").grid(row=0, column=0, sticky="W")
-lhs_entry = tk.Entry(tab1, textvariable=lhs_filepath, width=32)
+lhs = tk.StringVar()
+tk.Label(tab1, text="Left:").grid(row=0, column=0, sticky="W")
+lhs_entry = tk.Entry(tab1, textvariable=lhs, width=48)
+lhs_entry.insert(0, settings['lhs_filepath'])
 lhs_entry.grid(row=0, column=1)
 lhs_entry.focus()
 
 # Right entry box
-rhs_filepath = tk.StringVar()
-tk.Label(tab1, text="Second file:").grid(row=1, column=0, sticky="W")
-rhs_entry = tk.Entry(tab1, textvariable=rhs_filepath, width=32)
+rhs = tk.StringVar()
+tk.Label(tab1, text="Right:").grid(row=1, column=0, sticky="W")
+rhs_entry = tk.Entry(tab1, textvariable=rhs, width=48)
+rhs_entry.insert(0, settings['rhs_filepath'])
 rhs_entry.grid(row=1, column=1)
 
 # Output folder
 tk.Label(tab1, text="Output folder:").grid(row=2, column=0, sticky="W")
-output_folder = tk.StringVar()
-output_entry = tk.Entry(tab1, textvariable=output_folder, width=32)
+output = tk.StringVar()
+output_entry = tk.Entry(tab1, textvariable=output, width=48)
+output_entry.insert(0, settings['output_folder'])
 output_entry.grid(row=2, column=1)
 
 # Compare button
-compare_button = tk.Button(tab1, text="Compare", command=lambda: run_comparison(lhs_filepath.get(), rhs_filepath.get()))
+compare_button = tk.Button(tab1, text="Compare", command=lambda: run_comparison(lhs.get(), rhs.get(), output.get(),
+                                                                                settings, new_settings))
 compare_button.grid(row=3, columnspan=2)
 
 # Add padding to each widget
 for child in tab1.winfo_children():
     child.grid_configure(padx=5, pady=3)
+
+# todo: tab2 add delimiter button
 
 # Start GUI
 window.mainloop()
