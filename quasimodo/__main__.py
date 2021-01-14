d4a13ad37c7a5d9a052612421416767f4fd001c7
zchmielewska <zchmielewska@gmail.com>
2021-01-14 19:09:10 +0100

    use hidden import for openpyxl

--- quasimodo/__main__.py
+++ quasimodo/__main__.py
@@ -44,12 +44,12 @@ def main():
     window.title("quasimodo | compare tables")
 
     # Create tabs
-    tabControl = ttk.Notebook(window)
-    tab1 = ttk.Frame(tabControl)
-    tabControl.add(tab1, text="Main")
-    tabControl.pack(expand=1, fill="both")
-    tab2 = ttk.Frame(tabControl)
-    tabControl.add(tab2, text="Settings")
+    tab_control = ttk.Notebook(window)
+    tab1 = ttk.Frame(tab_control)
+    tab_control.add(tab1, text="Main")
+    tab_control.pack(expand=1, fill="both")
+    tab2 = ttk.Frame(tab_control)
+    tab_control.add(tab2, text="Settings")
 
     # Tab1 | Paths frame
     paths_frame = ttk.LabelFrame(tab1, text="Paths")
